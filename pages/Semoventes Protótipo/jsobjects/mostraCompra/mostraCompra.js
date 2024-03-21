401d6875e0b5787bd8ee2ec968a593849c59bd25
felipe <bac.smart.idea@gmail.com>
2024-03-21 18:51:54 +0000

    protótipo de cadastro de semovente v2

--- pages/Semoventes Protótipo/jsobjects/mostraCompra/mostraCompra.js
+++
@@ -1,9 +0,0 @@
-export default {
-	mostraCompra(){
-		if (Forma_Aquisicao.selectedOptionLabel == 'Compra'){
-			Valor_Compra.setVisibility(true)
-		} else {
-			Valor_Compra.setVisibility(false)
-		}
-	}
-}
