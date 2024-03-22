164d7de5c6e624c7d453fa1acc56fd300b70ac8c
felipe <bac.smart.idea@gmail.com>
2024-03-22 15:55:52 +0000

    Commit da versao usada na apresentacao

--- pages/Prontuário/jsobjects/LinkUrl/LinkUrl.js
+++ pages/Prontuário/jsobjects/LinkUrl/LinkUrl.js
@@ -2,7 +2,8 @@ export default {
 	url: "http://localhost:2337",
 	host_docker: "http://host.docker.internal:2337",
 	url_api: "http://host.docker.internal:2337",
-	url_App_Smith: "http://localhost:2337"
+	url_App_Smith: "http://172.19.145.29:2337",
+	url_App_Smithh: "http://localhost:2337"
 }
 /*
 PM
---
+++ pages/Semoventes Copy Copy/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	funcao_get_one_semovente_consulta () {
+	let todas_consultas = get_condutor.data
+	todas_consultas.data.filter()
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
