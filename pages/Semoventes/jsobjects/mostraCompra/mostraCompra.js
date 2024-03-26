28a42ba54cfa9a492577e35ea454adb1e3ac101d
felipe <bac.smart.idea@gmail.com>
2024-03-26 18:32:08 +0000

    Implementacao v1

--- pages/Semoventes/jsobjects/mostraCompra/mostraCompra.js
+++
@@ -1,9 +0,0 @@
-export default {
-	mostraCompra(){
-		if (Forma_AquisicaoCopy.selectedOptionLabel == 'Compra'){
-			Valor_Compra.setVisibility(true)
-		} else {
-			Valor_Compra.setVisibility(false)
-		}
-	}
-}
